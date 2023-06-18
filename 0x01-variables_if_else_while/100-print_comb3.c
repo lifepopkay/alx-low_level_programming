a8ba2df083be4a9b2c718e3da19a6328484e0b99
lifepopkay <popoolafolas@gmail.com>
2023-06-18 09:20:32 -0700

    Ctask

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,21 @@
  */
 int main(void)
 {
-        int k, m;
+	int k, m;
 
-        for (k = 0; k < 9; k++)
-        {
-                for (m = k + 1; m < 10; m++)
-                {
-                        putchar((k % 10) + '0');
-                        putchar((m % 10) + '0');
+	for (k = 0; k < 9; k++)
+	{
+		for (m = k + 1; m < 10; m++)
+		{
+			putchar((k % 10) + '0');
+			putchar((m % 10) + '0');
 
-                        if (k == 8 && m == 9)
-                                continue;
-
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar(10);
-        return (0);
+			if (k == 8 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return (0);
 }
