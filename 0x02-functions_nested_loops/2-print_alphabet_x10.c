5536cbe110ee332028565cb4bd0f32c16ee59915
lifepopkay <popoolafolas@gmail.com>
2023-06-21 16:22:34 -0700

    betty fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - prints 10 times the alphabet
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet_x10(void)
-
 {
-	char i, j;
+	int a, b;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar(j);
+			_putchar(b);
 		}
-	_putchar(10);
+	_putchar('\n');
 	}
 }
