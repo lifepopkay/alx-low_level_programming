67f94435a1d01b6fe853325be9b7eee6f7d40aab
lifepopkay <popoolafolas@gmail.com>
2023-06-23 01:49:38 -0700

    multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - check if a digit
+ * is positive or negative
+ *
+ * Return: Always 0 Succes
+ */
+
+void positive_or_negative(int i)
+{       
+        if (i > 0)
+        {       
+                printf("%d is positive\n", i);
+        }               
+        else if (i == 0)
+        {       
+                printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
