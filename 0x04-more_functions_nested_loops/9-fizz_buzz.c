e10d8f6b420194d0eb149fa0b58a926928ed031f
lifepopkay <popoolafolas@gmail.com>
2023-06-25 17:22:34 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,29 +6,19 @@
  */
 int main(void)
 {
-	int n;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	int i;
+
+	for (i = 1; i < 101; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf(" FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
-			printf(" Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf(" Buzz");
-		}
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else
-		{
-			printf(" %d", n);
-		}
+			printf("%d ", i);
 	}
-	printf("\n");
 	return (0);
 }
