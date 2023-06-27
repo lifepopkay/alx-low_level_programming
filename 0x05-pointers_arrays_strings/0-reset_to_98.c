707d84564a808ee2c7153ae7861ac3b54640aafa
lifepopkay <popoolafolas@gmail.com>
2023-06-27 07:35:16 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This updates a values
+ * @n: updated from function
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This task was on pointer and Arrays
