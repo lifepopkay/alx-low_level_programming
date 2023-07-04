f0d0319f146e5e539ad2c033b79844f9f78c6821
lifepopkay <popoolafolas@gmail.com>
2023-07-04 02:59:41 -0700

    compares string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,18 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while (*s1 == *s2)
 	{
-	       if (*s2 > *s1)
-		       return (*s1 - *s2);
-	       else if (*s1 > *s2)
-			return (*s1 - *s2);
-	       else
-		       return (0);
-		       
+		if (*s1 == '\0')
+			return (0);
+
 		s1++;
 		s2++;
 	}
-
-	return (0);
+	return (*s1 - *s2);
 }
