908f75b5e0ff7b41d1821504ea87825803a0a31e
lifepopkay <popoolafolas@gmail.com>
2023-07-07 14:36:49 -0700

    create a lib

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strncpy - copy a string
+ *
+ * @dest: destinstion
+ * @src: source
+ * @n: length
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i  < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
