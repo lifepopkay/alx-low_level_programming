9d430afd5bf5489be26d5a43ef5f7181ab354edf
lifepopkay <popoolafolas@gmail.com>
2023-07-08 14:46:30 -0700

    all arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this print all arguments
+ * @argc: argument count
+ * @argv: array of character
+ *
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	if (argc)
+	{
+		for  (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return 0;
+}
