7245eab333e58e7b97f75d863141d1f2ebb14a8b
lifepopkay <popoolafolas@gmail.com>
2023-07-18 04:32:56 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - detail of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of the dog's name
+ *
+ * Description: this task a structure that
+ * contains the details of a dog.
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
