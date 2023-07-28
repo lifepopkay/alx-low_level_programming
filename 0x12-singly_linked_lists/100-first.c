b0051fb453c22a22034538863108e80350fa2d4b
lifepopkay <popoolafolas@gmail.com>
2023-07-28 15:01:25 -0700

    The hare and the tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return
  */
 
-void _attribute_ ((constructor)) bmain()
+void __attribute__((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
