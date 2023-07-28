579f70a7e0def79a9bad5d2cfb8f92d5be7f8ed6
lifepopkay <popoolafolas@gmail.com>
2023-07-28 14:14:55 -0700

    singly list_1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * print_list - prints all the 
+ * element of a list
+ * @h: singly linked list used
+ *
+ * Return: number of element in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
