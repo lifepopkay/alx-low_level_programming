47038a82b43669ea4e06839a6cda6837e5797406
lifepopkay <popoolafolas@gmail.com>
2023-08-08 13:53:49 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int writtenFile;
+	int numOfLetters = 0;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+	{
+		return (-1);
+
+	if (text_content)
+	{
+		for (numOfLetters = 0; text_content[numOfLetters]; numOfLetters++)
+		{
+		;
+		writtenFile = write(fd, text_content, numOfLetters);
+		if (writtenFile == -1)
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
