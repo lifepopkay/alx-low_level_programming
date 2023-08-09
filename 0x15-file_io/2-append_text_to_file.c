416a9358d3c98971b522adc2c1e3f359db530232
lifepopkay <popoolafolas@gmail.com>
2023-08-08 20:23:05 -0700

    error fox

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,19 +17,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-	{
 		return (-1);
 
 	if (text_content)
 	{
 		for (numOfLetters = 0; text_content[numOfLetters]; numOfLetters++)
-		{
 		;
-		writtenFile = write(fd, text_content, numOfLetters);
-		if (writtenFile == -1)
-			return (-1);
-		}
 	}
+
+	writtenFile = write(fd, text_content, numOfLetters);
+	if (writtenFile == -1)
+		return (-1);
+
 
 	close(fd);
 	return (1);
