8e7d4c69ae39b5466e3cca5fcb1f0a491ffe4cfe
lifepopkay <popoolafolas@gmail.com>
2023-12-12 14:24:50 -0800

    real

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/*
+ * add - add two integers
+ * @a: first num
+ * @b: second num
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/*
+ * sub - subtract two integer
+ * @a: first num
+ * @b: second num
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/*
+ * mul - multiplies two intergers
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/*
+ * div - divides a num by another
+ * @a: first num
+ * @b: second num
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/*
+ * mod -find the modulus
+ * @a: first num
+ * @b: second num
+ * Return: modulus
+ */
+int mod(int a , int b)
+{
+	return (a % b);
+}
