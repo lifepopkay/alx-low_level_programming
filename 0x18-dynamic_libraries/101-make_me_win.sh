47522b1f725cf541eb600546fe0aa051bf5514c3
lifepopkay <popoolafolas@gmail.com>
2023-12-12 14:18:01 -0800

    hmm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/lifepopkay/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -p .. https://raw.githubusercontent.com/lifepopkay/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
+export LD_PRELOAD="$PWD/../libgm.so"
