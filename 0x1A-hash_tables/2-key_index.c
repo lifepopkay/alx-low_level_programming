db8226b6ee9a5ee14717f8b5a12c08045061ea79
lifepopkay <popoolafolas@gmail.com>
2024-01-03 02:25:33 -0800

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,13 +4,12 @@
  * key_index - function gives index of a key
  *
  * @key: the key of the value
- * @size: size the array of the hash table
- *
+ * @size: size the array of the hash tab *
  * Return: this uses the hash_djb2 
  */
 
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) * size);
+	return (hash_djb2(key) % size);
 }
