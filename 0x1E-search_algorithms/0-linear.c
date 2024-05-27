e4fb750d64ff66ed7a0ca36d510783ef7c12e396
lifepopkay <popoolafolas@gmail.com>
2024-05-28 00:17:10 +0100

    task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,11 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
-int linear_search(int *array, size_t size, int value)
-{
+int linear_search(int *array, size_t size, int value) {
     int i;
 
     if (array == NULL)
         return (-1);
-
     for (i = 0; i < (int)size; i++)
     {
         printf("Value checked array[%u] = [%d]\n", i, array[i]);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,8 +14,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
-{
+typedef struct listint_s {
     int n;
     size_t index;
     struct listint_s *next;
